5de73a98a05d134d38480823159656e5500862ef
Loy van Beek <loy.vanbeek@gmail.com>
2015-04-24 11:22:07 +0200

    Made dummy-proof question generator for speech_recognition challenge

--- GermanOpen2015/speech_recognition/pick_random_sentence.py
+++ GermanOpen2015/speech_recognition/pick_random_sentence.py
@@ -3,15 +3,66 @@
 import csv
 import random
 
+answering = {}
+
 with open("speech_recognition_questions.csv") as f:
     reader = csv.DictReader(f, delimiter=";")
 
     questions = list(reader)
+    random.shuffle(questions)
 
-    q = random.choice(questions)
+    for direct in range(5):
+        question_index = direct + 1
+        q = questions.pop()
+        dict_key = "{0}: {1}".format(question_index, q["Question"])
 
-    # print q
+        print "Question {0}: {1}.   ----> {2}".format(question_index, q["Question"], q["ANSWER"])
+        key = raw_input("Correct answer? c = correct, i = incorrect: ").strip()
 
-    print q["Question"]
-    print "----"
-    print q["ANSWER"]
+        if key == "c":
+            answering[dict_key] = 10
+        if key == "i":
+            answering[dict_key] = 0
+        else:
+            print "Key not allowed"
+
+
+        print answering
+        print "-" * 20
+        print
+
+    print "#" * 20
+
+    for indirect in range(5):
+        question_index = indirect + 1
+        q = questions.pop()
+
+        dict_key = "{0}: {1}".format(question_index, q["Question"])
+
+        print "Question {0}: {1}.   ----> {2}".format(question_index, q["Question"], q["ANSWER"])
+        key = raw_input("Correct answer? c = correct, i = incorrect, r = repeat: ")
+        if key == "c":
+            answering[dict_key] = 20
+        elif key == "i":
+            answering[dict_key] = 0
+        elif key == "r":
+            print "Question {0}m, 2nd attempt: {1}.   ----> {2}".format(question_index, q["Question"], q["ANSWER"])
+            key = raw_input("Correct answer in 2nd attempt? c = correct, i = incorrect: ")
+            if key == "c":
+                answering[dict_key] = 10
+            if key == "i":
+                answering[dict_key] = 0
+            else:
+                print "Key not allowed"
+        else:
+            print "Key not allowed"
+
+        print answering
+        print "-" * 20
+        print
+
+    total_score = sum(answering.values())
+    print "TOTAL SCORE: {0}".format(total_score)
+
+
+    
