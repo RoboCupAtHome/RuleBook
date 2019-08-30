f8f20a6a2b14200fd995f2e27b8de9fdb149953b
Alex Moriarty <amoriarty@fetchrobotics.com>
2019-08-30 15:42:20 -0700

    Fix docker compiler script & documentation
    - fix error that script didn't accept more than 2 arguments
    - fix missing quote in full compile example
    - fix typo pdf->tex in quick compile example

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 2 ]; then
   echo "usage: "
   echo "  "$0" [command] [filename]"
   echo "  Command uses https://github.com/blang/latex-docker see README for full details\n"
   echo "  quick pass:"
-  echo "    "$0" pdflatex Rulebook.pdf"
+  echo "    "$0" pdflatex Rulebook.tex"
   echo "  full build:"
-  echo "    "$0" /bin/sh -c \"pdflatex Rulebook.tex && makeindex Rulebook.tex && makeindex Rulebook.tex && pdflatex Rulebook.tex && pdflatex Rulebook.tex"
+  echo "    "$0" /bin/sh -c \"pdflatex Rulebook.tex && makeindex Rulebook.tex && makeindex Rulebook.tex && pdflatex Rulebook.tex && pdflatex Rulebook.tex\""
   exit 1
 fi
 
