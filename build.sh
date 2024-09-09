2ff39732b4f3083e53845f041f99305dd7e995f7
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2024-09-09 09:13:46 +0200

    update build instructuions, support podman

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+IMAGE=docker.io/texlive/texlive:TL2022-historic
+
+if command -v podman >/dev/null 2>&1
+then
+    podman run --rm --net=none -w /data/ -v`pwd`:/data -e TERM=xterm-256color $IMAGE make all
+else
+    docker run --rm --user="$(id -u):$(id -g)" --net=none -w /data/ -v`pwd`:/data -e TERM=xterm-256color $IMAGE make all
+fi
+
