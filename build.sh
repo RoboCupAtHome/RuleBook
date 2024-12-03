ca71d5a81b97287b7ba763cde2a6883f2e7b5049
Leroy Ruegemer <lruegeme@techfak.uni-bielefeld.de>
2024-12-03 16:46:45 +0100

    fixes build.sh and scoresheet macros

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ IMAGE=docker.io/texlive/texlive:TL2022-historic
 
 if command -v podman >/dev/null 2>&1
 then
-    podman run --rm --net=none -w /data/ -v`pwd`:/data -e TERM=xterm-256color $IMAGE make all
+    podman run --rm --net=none -w /data/ -v`pwd`:/data -e TERM=xterm-256color $IMAGE make rulebook scoresheets
 else
-    docker run --rm --user="$(id -u):$(id -g)" --net=none -w /data/ -v`pwd`:/data -e TERM=xterm-256color $IMAGE make all
+    docker run --rm --user="$(id -u):$(id -g)" --net=none -w /data/ -v`pwd`:/data -e TERM=xterm-256color $IMAGE make rulebook scoresheets
 fi
 
